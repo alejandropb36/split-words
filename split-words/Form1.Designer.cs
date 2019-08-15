bf4faebebededf65707888f70c9cbd16867c5d62
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-08-15 17:45:50 -0500

    Intial commit

--- split-words/Form1.Designer.cs
+++ split-words/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace split_words
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxA1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxA1
+            // 
+            this.textBoxA1.Location = new System.Drawing.Point(35, 72);
+            this.textBoxA1.Multiline = true;
+            this.textBoxA1.Name = "textBoxA1";
+            this.textBoxA1.Size = new System.Drawing.Size(278, 141);
+            this.textBoxA1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(849, 466);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxA1);
+            this.Name = "MainForm";
+            this.Text = "Split Words";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxA1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
